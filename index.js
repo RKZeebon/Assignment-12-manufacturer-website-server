fd27e8e5b6cf43c9d4b0bd1c1504aafa3e4d6519
RKZeebon <rkzeebon@gmail.com>
2022-05-23 03:00:12 +0600

    delete order done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -19,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db('masonTools').collection('tools')
+        const ordersCollection = client.db('masonTools').collection('orders')
 
         app.get('/tools', async (req, res) => {
             const tools = await toolsCollection.find().toArray()
@@ -26,9 +27,38 @@ async function run() {
             res.send(tools)
         })
 
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const q = { _id: ObjectId(id) }
+            const tool = await toolsCollection.findOne(q)
+            res.send(tool)
+        })
 
+        app.post('/orders', async (req, res) => {
+            const doc = req.body
+            const result = await ordersCollection.insertOne(doc)
+            res.send(result)
+        })
+        app.get('/myorders', async (req, res) => {
+            const email = req.query.email
+            const q = { email }
+            const result = await ordersCollection.find(q).toArray()
+            res.send(result)
+        })
 
+        app.get('/myorder/:id', async (req, res) => {
+            const id = req.params.id
+            const q = { _id: ObjectId(id) }
+            const result = await ordersCollection.findOne(q)
+            res.send(result)
+        })
 
+        app.delete('/myorder/:id', async (req, res) => {
+            const id = req.params.id
+            const q = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(q)
+            res.send(result)
+        })
 
 
     } finally {
